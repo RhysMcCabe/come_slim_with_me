8485777f9b2c98c52662667ad7f37cf952a18a40
Rhys <x00166574@mytudublin.ie>
2020-10-21 22:29:43 +0100

    Added foreign key to discussion linking topic. Topics now displayed in discussion

--- messageboard/models.py
+++ messageboard/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=140)
+    member = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    description = models.TextField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+
 class Discussion(models.Model):
     title = models.CharField(max_length=140)
     member = models.ForeignKey(
@@ -14,15 +27,4 @@ class Discussion(models.Model):
     def __str__(self):
         return self.title
 
-
-class Topic(models.Model):
-    name = models.CharField(max_length=140)
-    member = models.ForeignKey(
-        get_user_model(),
-        on_delete=models.CASCADE,
-    )
-
-    description = models.TextField(max_length=400)
-
-    def __str__(self):
-        return self.name
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
