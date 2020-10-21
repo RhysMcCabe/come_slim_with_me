650db7b0ff6a5b2013d00731ddefe12ab94c8d21
Rhys <x00166574@mytudublin.ie>
2020-10-21 23:13:05 +0100

    added topic names to discussions

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import DiscussionListView, DiscussionDetailView, TopicListView
 
 urlpatterns = [
-    path('discussion/', DiscussionListView.as_view(), name='discussion'),
+    path('topic/<int:pk>/', DiscussionListView.as_view(), name='discussion'),
     path('discussion/<int:pk>/', DiscussionDetailView.as_view(),
          name='discussion_detail'),
     path('', TopicListView.as_view(), name='topic'),
