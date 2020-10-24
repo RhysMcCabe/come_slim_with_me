f570bf0ec1b58d370c47cad8d89ba1d15c97b6e9
Rhys <x00166574@mytudublin.ie>
2020-10-24 22:15:19 +0100

    Solved issue with url on discussion_detail

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -10,12 +10,12 @@ from .views import(
 
 urlpatterns = [
     path('discussion/<int:pk>/edit/',
-    DiscussionUpdateView.as_view(), name='discussion_edit'),
+         DiscussionUpdateView.as_view(), name='discussion_edit'),
     path('discussion/<int:pk>/delete/',
-    DiscussionDeleteView.as_view(), name='discussion_delete'),
+         DiscussionDeleteView.as_view(), name='discussion_delete'),
     path('discussion/new/', DiscussionCreateView.as_view(), name='discussion_new'),
-    path('topic/<int:pk>/', DiscussionListView.as_view(), name='discussion'),
-    path('discussion/<int:pk>/', 
-    DiscussionDetailView.as_view(), name='discussion_detail'),
+    path('topic/<int:pk>/', DiscussionListView.as_view(), name='discussion_list'),
+    path('discussion/<int:pk>/',
+         DiscussionDetailView.as_view(), name='discussion_detail'),
     path('', TopicListView.as_view(), name='topic'),
 ]
