9062a62634c494df48eaf256ecf77fad95eb4d49
Lauren Mason <X00167132@mytudublin.ie>
2020-10-27 20:29:05 +0000

    Created games page with 3 games

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView, ContactUsView
+from .views import HomePageView, ContactUsView, GamesPageView, SnakePageView, TicTacToePageView, FlipMemoryPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('contactus/', ContactUsView.as_view(), name='contactus'),
+    path('games/', GamesPageView.as_view(), name='games'),
+    path('games/snake', SnakePageView.as_view(), name='snake'),
+    path('games/tictactoe', TicTacToePageView.as_view(), name='tictactoe'),
+    path('games/flipmemory', FlipMemoryPageView.as_view(), name='flipmemory'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,15 @@ class HomePageView(TemplateView):
 
 class ContactUsView(TemplateView):
     template_name = 'contactus.html'
+
+class GamesPageView(TemplateView):
+    template_name = 'games_base.html'
+
+class SnakePageView(TemplateView):
+    template_name = 'game_snake.html'
+
+class TicTacToePageView(TemplateView):
+    template_name = 'game_tictactoe.html'
+
+class FlipMemoryPageView(TemplateView):
+    template_name = 'game_flipmemory.html'
