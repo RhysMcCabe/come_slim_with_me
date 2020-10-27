1864fcc3cf4293a20cb068dd9bd3059acb163b37
Rhys <x00166574@mytudublin.ie>
2020-10-27 23:21:18 +0000

    Added authentication/ authorization for creating discussions/ topics, jumbotron, date created to topics

--- messageboard/models.py
+++ messageboard/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from datetime import datetime
 
 
 class Topic(models.Model):
@@ -12,12 +13,16 @@ class Topic(models.Model):
     )
 
     description = models.TextField(max_length=400)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('topics_detail', args=[str(self.id)])
+
+    def get_date_created(self):
+        return self.date_created.strftime('created: %d/%m/%y at %H:%M')
 
 
 class Discussion(models.Model):
