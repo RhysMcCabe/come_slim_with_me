f895d19f6111e8c13bc668c7ae5d07fc851f773b
Lauren Mason <X00167132@mytudublin.ie>
2020-12-10 17:20:55 +0000

    Added custom css

--- studentboard/urls.py
+++ studentboard/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('shop/', include('shop.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
